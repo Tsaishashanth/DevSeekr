0854891104d0ffd426547f2ad7c77fbdb4b18d2c
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-07-02 05:02:51 +0530

    updated search section

--- src/App.js
+++ src/App.js
@@ -15,6 +15,29 @@ function App() {
         <p>Powered by the Stack Overflow API, DevSeekr gives you real-time results right inside your workspace. Faster queries, smarter coding, zero context-switching.</p>
       </div>
     </div>
+
+    {/* second section */}
+
+    {/* main section */}
+    <div class="navbar">
+      <h1>Search</h1>
+    </div>
+    <div class ="container">
+      <div class="search">
+        <div class="type">
+          <p>Search Questions...</p>
+        </div>
+        <div class="button">
+          <button>Search</button>
+        </div>
+      </div>
+
+      <div class="contentcontainer">
+        <div class = "contentbutton">
+          <button>View On Stack Flow</button>
+        </div>
+      </div>
+    </div>
   </>
   );
 }
