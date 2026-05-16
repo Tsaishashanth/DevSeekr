2294e887067d7e06b8171c5251dc8e544ddcd6b0
Tsaishashanth <saishashanthtarigopula@gmail.com>
2026-05-16 16:29:15 +0530

    :feat: add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © 2026 DevSeekr. All rights reserved.
+      </p>
+
+      <hr />
+
+      <p>
+        Built for developers by Tarigopula Sai
+        Shashanth.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
